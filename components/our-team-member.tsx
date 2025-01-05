e41ff3ec16f3953da40c40c6f4fb11903eed8c40
Akshay <akshay.allen26200@gmail.com>
2025-01-05 10:36:50 +0530

    error getting hard

--- components/our-team-member.tsx
+++ components/our-team-member.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from "react"
+import React from "react"
 import { motion } from "framer-motion"
 import Image from "next/image"
 import { Facebook, Twitter, Linkedin } from "lucide-react"
